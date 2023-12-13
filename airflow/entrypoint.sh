ea5ecd92b035e4cb773dd05ffc0f41fce9fea6b9
Pascal Bürklin <pascal@buerkl.in>
2023-12-13 14:38:36 +0100

    Add conntype

--- airflow/entrypoint.sh
+++ airflow/entrypoint.sh
@@ -6,8 +6,18 @@
 airflow db init
 
 airflow users  create --role Admin --username admin --email admin --firstname admin --lastname admin --password admin
-# Start Airflow webserver
 
+# Add Spark connection
+if ! airflow connections get spark > /dev/null 2>&1; then
+    airflow connections add spark --conn-type Spark --conn-host spark://spark --conn-port 7077
+    echo "Spark connection added."
+else
+    echo "Spark connection already exists."
+fi
+
+
+
+# Start Airflow webserver
 airflow scheduler &
 
 exec airflow webserver
